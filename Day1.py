710858eec644aae1216df38a6a01fb2b11b913fa
Rohit Kumar <rg479270@gmail.com>
2025-05-11 23:42:53 +0530

    Initial commit

---
+++ Day1.py
@@ -0,0 +1,38 @@
+# Today i learnt Comments , Escape Sequences and Print
+
+
+
+print("I am Rohit")
+print("Who are you")
+
+'''
+Yah achha hai multiple
+line me comment likhna ho to 
+'''
+#or
+"""
+ye bhi use kar skte ho
+multiple line comment me 
+"""
+
+
+# single line k liy achha hai 
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+ 
+ #escape   means - \
+
+# print("This doesnt "execute") agr ye comment out kr de to error aa jayga
+print("This will \" execute")
+
+
+#print staement
+
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+print("Ram", 1,2,3, "Shayam", 5,6, end="22\n")
