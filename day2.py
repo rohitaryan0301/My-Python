a3d94749b6ddf980ea87464180fa0e692207110f
rohitaryan0301 <149464200+rohitaryan0301@users.noreply.github.com>
2025-05-12 22:19:44 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,58 @@
+# Today i learn 
+
+# variable, data type , list ,tuple and dict
+
+
+# Variable its like a container
+'''
+
+a = complex(2, 6)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+"""
+list jo hota hai wo collection of all data types 
+ye change ho skte hai matalb mutable hai
+"""
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+"""
+tuple ek prakar ka list hi hai jo 
+change nhi hota h mtlab immutable 
+"""
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+
+""" dictionary jaisa hai ye (Dict)
+"""
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+
+'''
+
+#WAP for Calculator
+
+ 
+a = 3
+b=6
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
